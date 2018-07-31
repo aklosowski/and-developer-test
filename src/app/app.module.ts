a40f4338929d71772a3e84232f2e4224a212c23f
Adam Klosowski <adam.karol.klosowski@gmail.com>
2018-07-31 16:26:32 +0100

    Basic project structure and CSS, hide secrets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { VenuesComponent } from './venues/venues.component';
+import { VenueComponent } from './venues/venue/venue.component';
+import { FoursquareService } from './foursquare.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VenuesComponent,
+    VenueComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [FoursquareService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
